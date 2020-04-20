45c128cba208fb222bf1384e5775217e8e64b2a0
Lucas Easley <lucaseasley11@gmail.com>
2020-04-20 14:07:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 # Your code goes here!
 class Dog
   def name=(dog_name)
-    @this_dogs_name = dog_name
+    @name = dog_name
   end
 
   def name
-    @this_dogs_name
+    @name
   end
-  class Dog
+  
+class Dog
     def bark
       puts "Woof!"
     end
