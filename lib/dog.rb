48d7ca7a8f7c55f9b5e0063659d75928b748649c
Lucas Easley <lucaseasley11@gmail.com>
2020-04-20 14:15:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,14 @@
-Class Dog
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
 end
-
-fido = Dog.new
-fido.name = "Fido"
-
-fido.name
-# => "Fido"
-
-fido.bark
-# => nil
